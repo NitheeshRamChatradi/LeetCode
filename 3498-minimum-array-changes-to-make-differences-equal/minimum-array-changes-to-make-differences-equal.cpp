18ba98a5c3671f597c5aa12f02812ceac216b450
Nitheesh Ram Chatradi <128576872+NitheeshRamChatradi@users.noreply.github.com>
2024-07-21 01:22:28 +0530

    Time: 127 ms (25.00%) | Memory: 111.8 MB (75.00%) - LeetSync

--- 3498-minimum-array-changes-to-make-differences-equal/minimum-array-changes-to-make-differences-equal.cpp
+++ 3498-minimum-array-changes-to-make-differences-equal/minimum-array-changes-to-make-differences-equal.cpp
@@ -18,16 +18,12 @@ public:
         }
         int ans = INT_MAX;
         for(auto &a: mp){
-            if(maxGap[a.first]==n/2){
-                int to_be_changed = n/2 - a.second;
-                ans = min(ans,to_be_changed);
-            }
-            else{
+            
                 int one_element_change = maxGap[a.first]-a.second;
                 int two_element_change = (n/2-maxGap[a.first])*2;
                 int total = one_element_change+two_element_change;
                 ans = min(ans,total);
-            }
+            
             
         }
         return ans;
