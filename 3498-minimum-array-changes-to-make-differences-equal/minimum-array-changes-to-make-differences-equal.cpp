a2132849a70b0fe63eba4c97fd0414b9fcf5623f
Nitheesh Ram Chatradi <128576872+NitheeshRamChatradi@users.noreply.github.com>
2024-07-21 00:58:25 +0530

    Time: 153 ms (25.00%) | Memory: 111.9 MB (75.00%) - LeetSync

---
+++ 3498-minimum-array-changes-to-make-differences-equal/minimum-array-changes-to-make-differences-equal.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minChanges(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int n = nums.size();
+        vector<int> maxGap(k+1,0);
+        for(int i=0;i<n/2;i++){
+            int diff = abs(nums[i]-nums[n-i-1]);
+            mp[diff]++;
+            int gap1 = k-min(nums[i],nums[n-i-1]);
+            int gap2 = max(nums[i],nums[n-i-1])-0;
+            int gap = max(gap1,gap2);
+            maxGap[gap]++;
+        }
+        for(int i=k-1;i>=0;i--){
+            maxGap[i] = maxGap[i]+maxGap[i+1];
+// If a differnce can be i+1 then it can also be i.
+        }
+        int ans = INT_MAX;
+        for(auto &a: mp){
+            if(maxGap[a.first]==n/2){
+                int to_be_changed = n/2 - a.second;
+                ans = min(ans,to_be_changed);
+            }
+            else{
+                int one_element_change = maxGap[a.first]-a.second;
+                int two_element_change = (n/2-maxGap[a.first])*2;
+                int total = one_element_change+two_element_change;
+                ans = min(ans,total);
+            }
+            
+        }
+        return ans;
+
+    }
+};
