c31e5782cefdaea5958b6f85d10fad1b4bc57930
Nitheesh Ram Chatradi <128576872+NitheeshRamChatradi@users.noreply.github.com>
2024-07-30 13:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (6.44%) - LeetSync

--- 1070-convert-to-base-2/convert-to-base-2.cpp
+++ 1070-convert-to-base-2/convert-to-base-2.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
     string baseNeg2(int n) {
-        string ans="";
-        while(n)
+        if (n == 0)
+        return "0";
+    int negBase=-2;
+    string converted = "";
+    while (n != 0)
+    {
+        // Get remainder by negative base, it can be
+        // negative also
+        int remainder = n % negBase;
+        n /= negBase;
+ 
+        // if remainder is negative, add abs(base) to
+        // it and add 1 to n
+        if (remainder < 0)
         {
-            ans=to_string(n&1)+ans;
-            n=-(n>>1);
+            remainder += (-negBase);
+            n += 1;
         }
-        return ans==""? "0" :ans;
+ 
+        // convert remainder to string add into the result
+        converted = to_string(remainder) + converted;
+    }
+ 
+    return converted;
     }
 };
