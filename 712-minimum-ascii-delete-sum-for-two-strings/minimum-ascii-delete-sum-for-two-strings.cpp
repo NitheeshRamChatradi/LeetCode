77977b9014446aab1f067b4713581023aa471c2f
Nitheesh Ram Chatradi <128576872+NitheeshRamChatradi@users.noreply.github.com>
2024-08-07 11:03:05 +0530

    Time: 12 ms (85.87%) | Memory: 17 MB (17.21%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -5,14 +5,22 @@ public:
         int sum1=0,sum2=0;
         for(auto i:s1) sum1+=i;
 		for(auto i:s2) sum2+=i;
+        for(int j=1;j<=s2.size();j++)
+        {
+            dp[0][j]=dp[0][j-1]+s2[j-1];
+        }
+        for(int i=1;i<=s1.size();i++)
+        {
+            dp[i][0]=dp[i-1][0]+s1[i-1];
+        }
         for(int i=1;i<=s1.size();i++)
         {
             for(int j=1;j<=s2.size();j++)
             {
                 if(s1[i-1]==s2[j-1])
-                dp[i][j]=dp[i-1][j-1]+s1[i-1];
+                dp[i][j]=dp[i-1][j-1];
                 else
-                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                dp[i][j]=min(dp[i-1][j]+s1[i-1],dp[i][j-1]+s2[j-1]);
             }
         }
         int ans=dp[s1.size()][s2.size()];
@@ -64,6 +72,6 @@ public:
         //     j--;
         //     cout<<s2[j];
         // }
-        return sum1+sum2-2*ans;
+        return ans;
     }
 };
